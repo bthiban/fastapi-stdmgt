c626a0abb930fde0e369780925437972be8b2b3d
bthiban <bthiban@gmail.com>
2025-04-14 22:48:08 +0100

    fixes for pylint

--- app/models/student.py
+++ app/models/student.py
@@ -4,11 +4,13 @@ import enum
 
 from app.core.database import Base
 
+# pylint: disable=too-few-public-methods
 class Gender(str, enum.Enum):
     male = "male"
     female = "female"
     other = "other"
 
+# pylint: disable=too-few-public-methods
 class Student(Base):
     __tablename__ = "students"
 
@@ -20,4 +22,4 @@ class Student(Base):
     email = Column(String(100))
     phone = Column(String(20))
     enrollment_year = Column(Integer)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now)
