3e89766fe61ed939ecabca9b567d3b10b5be6a1d
bthiban <bthiban@gmail.com>
2025-04-20 12:47:51 +0100

    db config

--- app/core/config.py
+++ app/core/config.py
@@ -9,4 +9,8 @@ DB_HOST = os.getenv("MYSQL_HOST")
 DB_PORT = os.getenv("MYSQL_PORT")
 DB_NAME = os.getenv("MYSQL_DB")
 
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# Fallback to SQLite in-memory for test/CI
+if all([DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME]):
+    DATABASE_URL = f"mysql+asyncmy://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+else:
+    DATABASE_URL = "sqlite+aiosqlite:///:memory:"
