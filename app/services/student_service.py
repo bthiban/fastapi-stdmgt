727e5dd5ba7eb095090cd5816ac46470ffe0380e
bthiban <bthiban@gmail.com>
2025-04-22 23:02:40 +0100

    add exist

--- app/services/student_service.py
+++ app/services/student_service.py
@@ -2,6 +2,7 @@ from app.models.student import Student
 from app.schemas.student_schema import StudentCreate, StudentUpdate
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from fastapi import HTTPException, status
 
 
 async def get_all_students(db: AsyncSession):
@@ -10,6 +11,15 @@ async def get_all_students(db: AsyncSession):
 
 
 async def create_student(db: AsyncSession, student_in: StudentCreate):
+    # check exist
+    result = await db.execute(select(Student).where(Student.email == student_in.email))
+    existing = result.scalars().first()
+    if existing:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Student with this email already exist.",
+        )
+
     student = Student(**student_in.model_dump())
     db.add(student)
     await db.commit()
